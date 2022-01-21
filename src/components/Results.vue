c53ea15d6570b429e4fc4c9b647f4302b8fdbec6
AphiweMp <aphiwempeto@gmail.com>
2022-01-21 14:36:25 +0200

    initial commit

---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+  <p>reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+  </div> 
+</template>
+
+<script>
+export default {
+    props:['score'],
+    data() {
+        return {
+            rank: null, 
+        }
+    },
+    mounted() {
+        if (this.score < 250){
+            this.rank = 'Ninja Fingers'}
+            else if (this.score < 480){
+                this.rank = 'Rapid Reflexes'
+            } else{
+                this.rank = "snail pace.."
+            }
+        }
+    }
+
+
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color:rgb(85, 185, 152) ;
+    font-weight: bold;
+}
+
+</style>
